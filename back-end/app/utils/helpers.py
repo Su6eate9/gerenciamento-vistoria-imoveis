4afa4bd12fff8981caa290b6a00088327b8965da
Isabelaodcm <150822385+Isabelaodcm@users.noreply.github.com>
2025-01-16 19:35:45 -0300

    Update helpers.py

--- back-end/app/utils/helpers.py
+++ back-end/app/utils/helpers.py
@@ -1,5 +1,13 @@
+import sqlite3
+
 def find_by_id(data_list, item_id):
     for item in data_list:
         if item.id == item_id:
             return item
     return None
+
+def get_db_connection():
+    #Cria e retorna uma conexão com o banco de dados SQLite.
+    conn = sqlite3.connect('prototipo1.db') 
+    conn.row_factory = sqlite3.Row  # Retorna os resultados como dicionários
+    return conn
